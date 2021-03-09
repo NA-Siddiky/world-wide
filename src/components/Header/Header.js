aea241fc4f60b9561bcbcb753d733dbf74428bba
NA-Siddiky <siddiky.academic@gmail.com>
2021-03-09 11:48:27 +0600

    removing header button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,9 +6,6 @@ const Header = () => {
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
             <Link className="nav-link" to="/">NavBar</Link>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
                 <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item">
